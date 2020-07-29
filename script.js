95c22dee1939e56a977a5380171c8ef31c5817d6
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-29 11:28:26 +0430

    7-4 test

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-var quizController = (function () {
-  var private = 10;
-  var privateFn = function (a) {
-    return a + private;
-  };
-  return { 
-      publicMethod:function(){
-      return privateFn(20);
-  } };
-})();
+/**********************************
+ **********QuIZ CONTROLLER*********
+ *********************************/
+ var quizController = (function () {
+localStorage.setItem('data',[1,2,3,4])
+  })();
+/**********************************
+ **********UI CONTROLLER*********
+ *********************************/
 var UIControler   =(function(){
-    var num1=30;
-    return{
-        sum:function(num2){
-            return num1+num2;
-        }
-    }
+ 
 })();
+/**********************************
+ ********** CONTROLLER*********
+ *********************************/
 var controller =(function (quizCtrl,UICtrl){
-    console.log(UICtrl.sum(100)+quizController.publicMethod);
+   
 })(quizController,UIControler);
