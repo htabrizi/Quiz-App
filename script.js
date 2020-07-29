0de27c7b4fe93afb6bb70a1b52d2b0d2bb36df0d
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-29 21:09:52 +0430

    7-5 Browser local storage

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /**********************************
- **********QuIZ CONTROLLER*********
+ **********QUIZ CONTROLLER*********
  *********************************/
  var quizController = (function () {
-localStorage.setItem('data',[1,2,3,4])
-  })();
+localStorage.setItem('data',JSON.stringify([1,2,3,4]));
+localStorage.removeItem('data');
+console.log(JSON.parse( localStorage.getItem('data')));
+})();
 /**********************************
  **********UI CONTROLLER*********
  *********************************/
