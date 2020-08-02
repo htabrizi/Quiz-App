e229854741adea32c6a2d7a943efad8de7ba24d7
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-02 14:34:26 +0430

    7-6 Add Questions

--- script.js
+++ script.js
@@ -2,19 +2,40 @@
  **********QUIZ CONTROLLER*********
  *********************************/
  var quizController = (function () {
-localStorage.setItem('data',JSON.stringify([1,2,3,4]));
-localStorage.removeItem('data');
-console.log(JSON.parse( localStorage.getItem('data')));
-})();
+ //*********** Question Constructor **************
+ function Question (id , questionText , options , correctAnswer ){
+  this.id=id;
+  this.questionText=questionText;
+  this.options=options ;
+  this.correctAnswer=correctAnswer;
+  return {
+   addQuestionOnLocalStorage:function(newQuestText,options){
+    console.log('hi')
+   }
+  }
+ }
+})();  
 /**********************************
  **********UI CONTROLLER*********
  *********************************/
-var UIControler   =(function(){
- 
+var UIControler =(function(){
+ var  domItems ={
+  //**************** Admin Panel Elements**************
+  questInsertBtn:document.getElementById("question-insert-btn"),
+  newQuestionText:document.getElementById("new-question-text"),
+  adminOptions:document.querySelectorAll(".admin-option")
+ };
+ return{
+  getDomItems : domItems
+  };
 })();
 /**********************************
  ********** CONTROLLER*********
  *********************************/
 var controller =(function (quizCtrl,UICtrl){
+ var selectedDomItems=UICtrl.getDomItems;
+selectedDomItems.questInsertBtn.addEventListener('click',function () {
+quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText,selectedDomItems.adminOptions);
+});
    
 })(quizController,UIControler);
