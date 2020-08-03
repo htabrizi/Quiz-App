33f3a8671b93cfbcc9b12eb6161528893f5fe869
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-08-03 14:02:40 +0430

    test

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 /**********************************
  **********QUIZ CONTROLLER*********
  *********************************/
- var quizController = (function () {
-localStorage.setItem('data',JSON.stringify([1,2,3,4]));
-localStorage.removeItem('data');
-console.log(JSON.parse( localStorage.getItem('data')));
+var quizController = (function () {
+  //************Question Cnstructor*******/
+  function Question(id, questionText, options, correctAnswer) {
+    this.id = id;
+    this.questionText = questionText;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+  }
+  return {
+    addQuestionOnLocalStorage: function (newQuestText, opts) {
+      var optionsArr, corrAns,questionId,newQuestion;
+      optionsArr = [];
+      questionId=0;
+      for (var i = 0; i < opts.length; i++) {
+        if (opts[i].value !== "") {
+          optionsArr.push(opts[i].value);
+        }
+        if (opts[i].previousElementSibling.checked && opts[i] !== "") {
+          corrAns = opts[i].value;
+        }
+      }
+      console.log(corrAns);
+    }
+  };
 })();
 /**********************************
  **********UI CONTROLLER*********
  *********************************/
-var UIControler   =(function(){
- 
+var UIControler = (function () {
+  var domItems = {
+    //********Admin Panel Elements********/
+    questInsertBtn: document.getElementById("question-insert-btn"),
+    newQuestionText: document.getElementById("new-question-text"),
+    adminOptions: document.querySelectorAll("admin-optio")
+  };
+  return {
+    getDomItems: domItems
+  };
 })();
 /**********************************
  ********** CONTROLLER*********
  *********************************/
-var controller =(function (quizCtrl,UICtrl){
-   
-})(quizController,UIControler);
+var controller = (function (quizCtrl, UICtrl) {
+
+  var selectedDomItems = UICtrl.getDomItems;
+  selectedDomItems.questInsertBtn.addEventListener('click', function () {
+    quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOptions)
+  });
+})(quizController, UIControler);
