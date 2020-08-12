246b0a25c3468790afc86f450af43a8ec0a11d1a
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-08-12 15:29:15 +0430

    7-9 Finish Add Question

--- script.js
+++ script.js
@@ -16,58 +16,58 @@ var quizController = (function () {
     getQuestionCollection: function () {
       return JSON.parse(localStorage.getItem('questionCollection'));
     },
-removeQuestionCollection:function(){
-  localStorage.removeItem('questionCollection');
-} 
+    removeQuestionCollection: function () {
+      localStorage.removeItem('questionCollection');
+    }
   };
   return {
     addQuestionOnLocalStorage: function (newQuestText, opts) {
-      var optionsArr, corrAns, questionId, newQuestion, getStoredQuests,isChecked;
-      if(questionLocalStorage.getQuestionCollection()===null){
+      var optionsArr, corrAns, questionId, newQuestion, getStoredQuests, isChecked;
+      if (questionLocalStorage.getQuestionCollection() === null) {
         questionLocalStorage.setQuestionCollection([]);
       }
       optionsArr = [];
- isChecked=false;
+      isChecked = false;
       for (var i = 0; i < opts.length; i++) {
         if (opts[i].value !== "") {
           optionsArr.push(opts[i].value);
         }
         if (opts[i].previousElementSilbling.checked && opts[i].value !== "") {
           corrAns = opts[i].value;
-          isChecked=true;
+          isChecked = true;
         }
-      } 
+      }
       //[{id: 0}, {id: 1}]
-      if(questionLocalStorage.getQuestionCollection().length>0){
-        questionId=questionLocalStorage.getQuestionCollection()[questionLocalStorage.getQuestionCollection().length-1].id+1;
-      }else{
-        questionId=0;
+      if (questionLocalStorage.getQuestionCollection().length > 0) {
+        questionId = questionLocalStorage.getQuestionCollection()[questionLocalStorage.getQuestionCollection().length - 1].id + 1;
+      } else {
+        questionId = 0;
       }
 
-      if(newQuestText.value !==""){
-        if(optionsArr.lenght>1){
-          if (isChecked){
-      newQuestion = new Question(questionId, newQuestionText.value, optionsArr, corrAns);
-      getStoredQuests =questionLocalStorage.getQuestionCollection();
-      getStoredQuests.push(newQuestion);
-      questionLocalStorage.setQuestionCollection(getStoredQuests);
-      newQuestionText.value="";
-      for (var x =0;x<opts.length;x++){
-        opts[x].value="";
-        opts[x].previousElementSilbling.checked = false;
+      if (newQuestText.value !== "") {
+        if (optionsArr.lenght > 1) {
+          if (isChecked) {
+            newQuestion = new Question(questionId, newQuestionText.value, optionsArr, corrAns);
+            getStoredQuests = questionLocalStorage.getQuestionCollection();
+            getStoredQuests.push(newQuestion);
+            questionLocalStorage.setQuestionCollection(getStoredQuests);
+            newQuestionText.value = "";
+            for (var x = 0; x < opts.length; x++) {
+              opts[x].value = "";
+              opts[x].previousElementSilbling.checked = false;
 
-      }
-      console.log(questionLocalStorage.getQuestionCollection());
-          }else{
+            }
+            console.log(questionLocalStorage.getQuestionCollection());
+          } else {
             alert('You missed to check correctly answer,or you checked answer without value.')
           }
-    }else{
-      alert('You must insert at least tow options ')
+        } else {
+          alert('You must insert at least tow options ')
+        }
+      } else {
+        alert('Please insert Question:');
+      }
     }
-    } else{
-      alert('Please insert Question:');
-    }
-  }
   };
 })();
 /**********************************
